733503944855d752d31b9aa4f6e7f488c5dde510
David Pham <David.Pham@selecthealth.org>
2016-05-04 13:16:14 -0600

    Added camera functionality.

---
+++ RaysHotDogs/TakePictureActivity.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Media;
+using Android.OS;
+using Android.Provider;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.IO;
+using RaysHotDogs.Utility;
+
+namespace RaysHotDogs
+{
+    [Activity(Label = "TakePictureActivity")]
+    public class TakePictureActivity : Activity
+    {
+        private ImageView rayPictureImageView;
+        private Button takePictureButton;
+        private File imageDirectory;
+        private File imageFile;
+        private Bitmap imageBitmap;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            
+            // Create your application here
+            SetContentView(Resource.Layout.TakePictureView);
+            FindViews();
+            HandleEvents();
+
+            imageDirectory = new File(Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryPictures),"RaysHotDogs");
+
+            if (!imageDirectory.Exists())
+            {
+                imageDirectory.Mkdirs();
+            }
+        }
+
+        private void FindViews()
+        {
+            rayPictureImageView = FindViewById<ImageView>(Resource.Id.rayPictureImageView);
+            takePictureButton = FindViewById<Button>(Resource.Id.takePictureButton);
+        }
+
+        private void HandleEvents()
+        {
+            takePictureButton.Click += TakePictureButton_Click;
+        }
+
+        private void TakePictureButton_Click(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            imageFile = new File(imageDirectory, String.Format("PhotoWithRay_{0}", Guid.NewGuid()));
+            intent.PutExtra(MediaStore.ExtraOutput, Android.Net.Uri.FromFile(imageFile));
+            StartActivityForResult(intent, 0);
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            int height = rayPictureImageView.Height;
+            int width = rayPictureImageView.Width;
+            imageBitmap = ImageHelper.GetImageBitMapFromFilePath(imageFile.Path, width, height);
+
+            if (imageBitmap != null)
+            {
+                rayPictureImageView.SetImageBitmap(imageBitmap);
+                imageBitmap = null;
+            }
+
+            GC.Collect();
+        }
+    }
+}
--- RaysHotDogs/Utility/ImageHelper.cs
+++ RaysHotDogs/Utility/ImageHelper.cs
@@ -31,5 +31,25 @@ namespace RaysHotDogs.Utility
 
             return imageBitmap; 
         }
+
+        public static Bitmap GetImageBitMapFromFilePath(string fileName, int width, int height)
+        {
+            BitmapFactory.Options options = new BitmapFactory.Options { InJustDecodeBounds = true};
+            BitmapFactory.DecodeFile(fileName, options);
+
+            int outHeight = options.OutHeight;
+            int outWidth = options.OutWidth;
+            int inSampleSize = 1;
+
+            if (outHeight > height || outWidth > width)
+            {
+                inSampleSize = outWidth > outHeight ? outHeight / height : outWidth / width;
+            }
+
+            options.InSampleSize = inSampleSize;
+            options.InJustDecodeBounds = false;
+            Bitmap resizedBitmap = BitmapFactory.DecodeFile(fileName, options);
+            return resizedBitmap; 
+        }
     }
 }
